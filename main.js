cce7cf9b5096a8edd181208573a9af1b53b5ec10
Demo User <demouser@MacBook-Pro.local>
2015-07-23 09:49:58 -0700

    two boxes flip successfully

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("hi");
+console.log("new");
+$(function() {
+  $("#card-1").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+  $("#card-2").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+  $("#card-3").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+});
